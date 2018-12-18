4099235f51c31f44ebbb75942a340e614a90e69e
sseul <jjwisg@naver.com>
2018-12-18 16:28:49 +0900

    1comm

---
+++ testtest/src/testtest/TestFrm.java
@@ -0,0 +1,11 @@
+package testtest;
+
+import javax.swing.JFrame;
+
+public class TestFrm extends JFrame{
+    public TestFrm() {
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
